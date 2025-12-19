31d15130b7364dde6e92c98016db8a1987025793
Rudra-cryptx <rudy.77goswami@gmail.com>
2025-12-19 15:52:34 +0530

    .

--- tinkler tutorial/3.py
+++ tinkler tutorial/3.py
@@ -1,11 +1,18 @@
 import tkinter as tk
-
 root = tk.Tk()
-root.geometry("500x500")
+root.geometry("500x900")
+def button():
+    user_text = lable.get()
+    result.config(text=f"your Typed text {user_text}")
+root.title("This is try")
 
-lable = tk.Label(root, text=("This is 3rd program.") , font=("Arial", 22))
-lable.pack()
+lable = tk.Entry(root,   font=("Arile" , 20))
+lable.pack(pady=10)
 
-enter = tk.Entry(root , font=("Arial" , 18) , width=40)
-enter.pack()
+btn = tk.Button(root , text="Submit" , font=("Arile" , 18) , command=button) 
+btn.pack()
+
+result = tk.Label(root, text="" , font=("Arile", 24))
+result.pack(pady=20)
+
 root.mainloop()
